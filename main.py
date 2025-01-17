371fb7d5a4c336c9afecacc6a3d5278da99c3e45
McGruning <markus.gruning@outlook.com>
2025-01-17 18:19:50 +1100

    B

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def sort_on(d):
 
 
 def chars_dict_to_sorted_list(num_chars_dict):
-    sorted_list =[]
+    sorted_list = []
     for ch in num_chars_dict:
         sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
     sorted_list.sort(reverse=True, key=sort_on)
